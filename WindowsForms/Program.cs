28a36554116a1297867a9914d461e9ee7329a9f7
kolli satya sai sandeep <kollisatyasaisandeep@gmail.com>
2024-12-28 11:02:55 +0530

    Add project files.

---
+++ WindowsForms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms
+{
+    class Program
+    {
+        
+        static double ConvertToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        static void Main()
+        {
+            
+            Console.Write("Enter Cel Temp: ");
+            double celTemp = Convert.ToDouble(Console.ReadLine());
+
+           
+            double fahrenheitTemp = ConvertToFahrenheit(celTemp);
+
+            
+            Console.WriteLine("Fahrenheit Temperature is : " + fahrenheitTemp);
+        }
+    }
+}
